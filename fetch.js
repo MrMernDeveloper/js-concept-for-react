f54210482f4ed4953da680890ec342d04bcfd35f
MrMernDeveloper <rakibkst16@gmail.com>
2022-09-12 19:48:40 +0600

    added some file

--- fetch.js
+++ fetch.js
@@ -23,11 +23,36 @@ fetch('url')
 
 const keys = Object.keys(student);
 const values = Object.values(student);
-console.log(keys);
-console.log(values);
+// console.log(keys);
+// console.log(values);
 
 // for 
 const numbers = [23, 54, 78, 989, 565];
 numbers.forEach(number => console.log(number));
 const multipliedNumber = numbers.map(number => number * 2);
-console.log(multipliedNumber);
+// console.log(multipliedNumber);
+
+// for of on array like object
+//loop on anj object using for in
+
+// add or remove from an array
+const products = [
+    { name: 'laptop', price: 32000, brand: 'lenovo', color: 'silver' },
+    { name: 'Phone', price: 200, brand: 'nokia', color: 'black' },
+    { name: 'watch', price: 100, brand: 'Xiomi', color: 'black' },
+    { name: 'sunGlass', price: 150, brand: 'husuk', color: 'blue' },
+    { name: 'camera', price: 25000, brand: 'samsung', color: 'golden' }
+];
+const newProduct = { name: 'webcam', price: 700, brand: 'LAL' }
+
+// copy products array and then add  newProduct
+const newProducts = [...products, newProduct];
+// console.log(newProducts)
+
+
+//create a new array without one specific item
+// remove phone means create a new array without the phone  values
+const remaining = products.filter(product => product.name !== 'Phone');
+console.log(remaining);
+
+
